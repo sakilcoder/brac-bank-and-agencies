33d9c1e5dc7f108f199b0372f344d01cdc2bcd71
Sakil Ahmed <sahmed@esrisa.com>
2024-01-14 11:51:09 +0600

    aoi added

---
+++ assets/aoi.js
@@ -0,0 +1,8 @@
+let aoi = {
+"type": "FeatureCollection",
+"name": "bd-ext1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "World Bounding Box (excluding Bangladesh)" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -180.0, 90.0 ], [ 180.0, 90.0 ], [ 180.0, -90.0 ], [ -180.0, -90.0 ], [ -180.0, 90.0 ] ], [ [ 86.988900033453859, 18.56034876899362 ], [ 95.121393221464757, 18.667637597331499 ], [ 94.971188861791745, 27.980307897059017 ], [ 86.645575782772667, 27.701356943380546 ], [ 86.988900033453859, 18.56034876899362 ] ] ] ] } }
+]
+}
